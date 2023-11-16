923cc4d61071ccc8513140cf95a343df2c96f366
rad16x <aradix@student.42.fr>
2023-11-16 22:07:11 +0100

    refactor: clean code + improve ft_memset

--- part1/test_ft_memset.c
+++ part1/test_ft_memset.c
@@ -6,48 +6,60 @@
 /*   By: aradix <aradix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 15:19:31 by aradix            #+#    #+#             */
-/*   Updated: 2023/11/15 18:21:47 by aradix           ###   ########.fr       */
+/*   Updated: 2023/11/16 22:04:00 by aradix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "check-my-libft.h"
 
+static bool	cmp_output(char *s, char *expected_s, int c, size_t size, size_t cmp_size)
+{
+	char	*ret;
+	char	*expected_ret;
+
+	ret = ft_memset(s, c, size);
+	expected_ret = memset(expected_s, c, size);
+	if (ret == NULL && expected_ret == NULL)
+		return (true);
+	if ((ret == NULL && expected_ret != NULL) || (ret != NULL
+			&& expected_ret == NULL))
+		return (false);
+	if (memcmp(ret, expected_ret, cmp_size) == 0)
+		return (true);
+	return (false);
+}
+
 void	test_ft_memset(void)
 {
-	char	*str;
-	char	*expected_str;
+	char	*s;
+	char	*expected_s;
 
-	str = (char *)malloc(sizeof(char) * 10);
-	if (!str)
-		return ;
-	expected_str = (char *)malloc(sizeof(char) * 10);
-	if (!expected_str)
+	s = (char *)malloc(sizeof(char) * 10);
+	expected_s = (char *)malloc(sizeof(char) * 10);
+	if (s == NULL || expected_s == NULL)
 	{
-		free(str);
-		return ;
+		free(s);
+		free(expected_s);
+		perror("Memory allocation error\n");
+		exit(EXIT_FAILURE);
 	}
-	/* TEST WITH SINGLE CHAR */
-	str = ft_memset(str, 163, 1);
-	expected_str = memset(str, 163, 1);
-	if (*str == *expected_str)
-		printf("%s [OK]", GREEN);
+	/* -------------------- TEST 01 -------------------- */
+	if (cmp_output(s, expected_s, 163, 1, 10))
+		printf("%s 1:[OK]", GREEN);
 	else
-		printf("%s [KO]", RED);
-	/* TEST WITH BASIC VALUE */
-	str = ft_memset(str, 'e', 10);
-	expected_str = memset(str, 'e', 10);
-	if (memcmp(str, expected_str, 10) == 0)
-		printf("%s [OK]", GREEN);
+		printf("%s 1:[KO]", RED);
+	/* -------------------- TEST 02 -------------------- */
+	if (cmp_output(s, expected_s, 'e', 10, 10))
+		printf("%s 2:[OK]", GREEN);
 	else
-		printf("%s [KO]", RED);
-	/* WITH NEGATIVE VALUE */
-	str = ft_memset(str, -120, 3);
-	expected_str = memset(str, -120, 3);
-	if (memcmp(str, expected_str, 10) == 0)
-		printf("%s [OK]", GREEN);
+		printf("%s 2:[KO]", RED);
+	/* -------------------- TEST 03 -------------------- */
+	if (cmp_output(s, expected_s, -120, 3, 10))
+		printf("%s 3:[OK]", GREEN);
 	else
-		printf("%s [KO]", RED);
-	/* free here */
-	free(expected_str);
+		printf("%s 3:[KO]", RED);
+	/* -------------------------------------------------- */
+	free(s);
+	free(expected_s);
 	printf("\x1b[0m\n");
 }
