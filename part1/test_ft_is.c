6b26033f4f54f355ad561db7c0d278932d886b8b
rad16x <aradix@student.42.fr>
2023-11-16 22:21:12 +0100

    refactor: clean code for ft_is*

--- part1/test_ft_is.c
+++ part1/test_ft_is.c
@@ -6,40 +6,43 @@
 /*   By: aradix <aradix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 12:51:27 by aradix            #+#    #+#             */
-/*   Updated: 2023/11/15 18:21:47 by aradix           ###   ########.fr       */
+/*   Updated: 2023/11/16 22:20:19 by aradix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "check-my-libft.h"
 
+static bool	cmp_output(int c, int (*implem)(int), int (*original)(int))
+{
+	int	ret;
+	int	expected_ret;
+
+	ret = implem(c);
+	expected_ret = original(c);
+	if ((ret == 0 && expected_ret == 0) || (ret > 0 && expected_ret > 0))
+		return (true);
+	return (false);
+}
+
 void	test_ft_is(int (*implem)(int), int (*original)(int))
 {
 	int	c;
-	int	value;
-	int	expected_value;
 
-	/* TEST WITH ASCII */
+	/* -------------------- TEST 01 -------------------- */
 	c = 0;
-	while (c <= 127)
-	{
-		value = implem(c);
-		expected_value = original(c);
-		if ((value == 0 && expected_value != 0) || (value > 0
-				&& expected_value <= 0))
-			break ;
+	while (c <= 127 && cmp_output(c, implem, original))
 		c++;
-	}
-	(c == 128) ? printf("%s [OK]", GREEN) : printf("%s [KO]", RED);
-	/* TEST WITH EXTENDED ASCII */
-	while (c <= 200)
-	{
-		value = implem(c);
-		expected_value = original(c);
-		if ((value == 0 && expected_value != 0) || (value > 0
-				&& expected_value <= 0))
-			break ;
+	if (c == 128)
+		printf("%s 1:[OK]", GREEN);
+	else
+		printf("%s 1:[KO]", RED);
+	/* -------------------- TEST 02 -------------------- */
+	while (c <= 200 && cmp_output(c, implem, original))
 		c++;
-	}
-	(c == 201) ? printf("%s [OK]", GREEN) : printf("%s [KO]", RED);
+	if (c == 201)
+		printf("%s 2:[OK]", GREEN);
+	else
+		printf("%s 2:[KO]", RED);
+	/* -------------------------------------------------- */
 	printf("\x1b[0m\n");
 }
