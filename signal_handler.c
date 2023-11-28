ed7b0b931fb975ef2d7dc1772c366a4f3f5a7363
Antoine Radix <aradix@student.42.fr>
2023-11-28 22:31:42 +0100

    refactor: remove useless file and code

--- signal_handler.c
+++ signal_handler.c
@@ -6,18 +6,16 @@
 /*   By: aradix <aradix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 22:45:39 by aradix            #+#    #+#             */
-/*   Updated: 2023/11/28 21:10:17 by aradix           ###   ########.fr       */
+/*   Updated: 2023/11/28 22:30:02 by aradix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "check-my-libft.h"
 
-/* jmp_buf	env; */
 sigjmp_buf	env;
 
 void	signal_handler(int signo)
 {
 	(void)signo;
-	/* longjmp(env, 1); */
 	siglongjmp(env, 1);
 }
